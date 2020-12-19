35cafaac904da5cd612878489353d5bf6359f608
prodriguezo <43780279+prodriguezo@users.noreply.github.com>
2020-12-19 10:44:39 +0100

    Eje funciones 1

---
+++ fundamentos-de-programacion-bikram-array-funciones/index.js
@@ -0,0 +1,12 @@
+function suma(n1,n2){
+    return (n1+n2);
+}
+function potenciacion(n1,n2){
+    let i=0;
+    let m=n1;
+    while(i<(n2-1)){
+        n1*=m;
+        i++;
+    }
+    return (n1);
+}
