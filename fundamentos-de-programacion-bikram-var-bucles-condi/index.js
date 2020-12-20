bab655b98f4c65ae3cf04f80b64f25b1646842b5
prodriguezo <43780279+prodriguezo@users.noreply.github.com>
2020-12-20 15:13:52 +0100

    versión 2

---
+++ fundamentos-de-programacion-bikram-var-bucles-condi/index.js
@@ -0,0 +1,50 @@
+let variableSinValor;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = PI * 2;
+booleanoAnd = booleano1 && booleano2;
+booleanoNot = !booleano1
+booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+incrementarDesp = 2;
+resultadoDesp = incrementarDesp++;
+incrementarAntes = 2;
+resultadoAntes = ++incrementarAntes;
+let variableValorNumerico = 1;
+const MiNombre = "tu nombre";
+const MiNumeroFav = 1;
+booleanoOr = booleano1 || booleano2;
+booleanoMix1 = (booleano1 && (TAU/2 === PI)) || (variableValorNumerico >= MiNumeroFav);
+seisNoEsNueve = (6 !== 9);
+booleanoMix2 = (variableValorNumerico < 0) < (MiNumeroFav * TAU);
+valorSuma = MiNumeroFav + variableValorNumerico;
+valorResta = MiNumeroFav - variableValorNumerico;
+valorMultiplicacion = MiNumeroFav * variableValorNumerico;
+valorDivision = MiNumeroFav / 3;
+let contarHasta10_2;
+for(contarHasta10_2 = 0;contarHasta10_2 <= 9;contarHasta10_2++){}
+let postI=0;
+let postJ=0;
+while(postJ<11){
+    postJ++;
+    postI+=postJ;
+}
+let sumaPares=0;
+for(let i=0;i<10;i++){
+    if(i%2===0)
+    {
+        sumaPares+=i;
+    }
+}
+let contarHasta10=0;
+while(contarHasta10<=10){
+    contarHasta10++;
+}
+let sumaImpares = 0;
+for(let i=0;i<10;i++){
+    if(i%2===0)
+    {
+        sumaImpares+=i;
+    }
+}
+
